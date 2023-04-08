ab1364abee3fec886cac6d7d2ad7d6af5e8923fe
Ashraf-Hessen <101559120+Ashraf-Hessen@users.noreply.github.com>
2023-04-09 00:30:27 +0200

    Edit on files

--- js/main.js
+++ js/main.js
@@ -1,16 +1,27 @@
 $(document).ready(function () {
-  $(".collections-list.owl-carousel").owlCarousel({
+  "use strict";
+
+  // Adjust Slider Height
+  var winH = $(window).height(),
+    navH = $(".navbar").innerHeight();
+  $(".hero").height(winH - navH);
+
+  $(".partners .owl-carousel").owlCarousel({
     loop: true,
     margin: 10,
     responsiveClass: true,
     dots: false,
     responsive: {
       0: {
-        items: 1.3,
+        items: 3,
+        nav: false,
+      },
+      768: {
+        items: 4,
         nav: false,
       },
       1000: {
-        items: 2.3,
+        items: 6,
         nav: false,
         loop: true,
         autoplay: true,
@@ -20,62 +31,28 @@ $(document).ready(function () {
     },
   });
 
-  $(".filter.owl-carousel").owlCarousel({
-    loop: false,
-    margin: 5,
-    responsiveClass: true,
-    dots: false,
-    autoWidth: true,
-    responsive: {
-      0: {
-        items: 4,
-        nav: false,
-      },
-      678: {
-        items: 6,
-        nav: false,
-      },
-      1000: {
-        items: 11,
-        nav: false,
-      },
-    },
-  });
-  $(".images-list.owl-carousel").owlCarousel({
-    loop: false,
+  $(".projects .owl-carousel").owlCarousel({
+    loop: true,
     margin: 10,
     responsiveClass: true,
     dots: false,
     responsive: {
       0: {
-        items: 4,
+        items: 1.1,
         nav: false,
       },
+      768: {
+        items: 3.5,
+        nav: false,
+      },
+      1000: {
+        items: 4.3,
+        nav: false,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        autoplayHoverPause: true,
+      },
     },
-  });
-
-  $(".amount .plus").click(function () {
-    $(this)
-      .siblings("input")
-      .val(parseInt($(this).siblings("input").val()) + 1);
-  });
-  $(".amount .minus").click(function () {
-    if ($(this).siblings("input").val() > 1) {
-      $(this)
-        .siblings("input")
-        .val(parseInt($(this).siblings("input").val()) - 1);
-    }
-  });
-  $(".product-details .main-image").height(
-    $(".product-details .main-image").width()
-  );
-  $(".product-details .small-image").height(
-    $(".product-details .small-image").width()
-  );
-  $(".product-details .small-image").click(function () {
-    $(".product-details .main-image img").attr(
-      "src",
-      $(this).children("img").attr("src")
-    );
   });
 });
